d3ded8c9aa3c88c65b4409b3f8f0e5565f2cb532
CaroleSinou <32440327+CaroleSinou@users.noreply.github.com>
2021-10-14 15:22:01 -0400

    Test change map config

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,29 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5386 }
+  rootPredicate: { 
+    "type": "or",
+    "predicates": [
+      {type: 'equals',
+       key: 'taxonKey', 
+       value: 5386 
+      },
+      {"type": "and",
+        "predicates": [
+          {
+            key: 'taxonKey',
+            type: 'equals',
+            value: 5386
+          },
+          {
+            key: "notIssues",
+            type: "equals",
+            value: "COUNTRY_COORDINATE_MISMATCH"
+          }
+        ]
+      }
+  ]
+ } 
+  highlightedFilters: ['taxonKey', 'gadmGid', 'stateProvince', 'basisOfRecord', 'typeStatus', 'cluster', 'recordedBy','publishingOrg', 'datasetName','occurrenceIssue'],
+  occurrenceSearchTabs: ['TABLE', 'MAP','GALLERY', 'DATASETS']
 };
