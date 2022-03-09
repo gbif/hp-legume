115734c8b64faa7a5eea0567ede8c6a4d4f0fc4d
Morten Høfft <mhoefft@gbif.org>
2022-03-09 14:22:38 +0100

    feat: update configuration schema. See https://github.com/gbif/hosted-portals/issues/221

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,15 +1,26 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary
+  }
+});
 
 var siteConfig = {
-  rootPredicate: { 
-    "type":"and",
-    "predicates": [
-      {"type":"equals","key":"taxonKey","value":"5386"},
-      {"type":"equals","key":"hasGeospatialIssue","value":"false"}
-    ]
+  routes: {
+    occurrenceSearch: {
+      // The route you are currently using for occurrence search. The language prefix will be added automatically
+      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
+      route: '/data'
+    }
   },
-  highlightedFilters: ['taxonKey', 'recordedBy', 'country', 'stateProvince', 'gadmGid', 'datasetKey', 'basisOfRecord', 'typeStatus', 'isInCluster','occurrenceIssue'],
-  occurrenceSearchTabs: ['TABLE', 'MAP','GALLERY', 'DATASETS']
+  occurrence: {
+    rootPredicate: {
+      "type": "and",
+      "predicates": [
+        { "type": "equals", "key": "taxonKey", "value": "5386" },
+        { "type": "equals", "key": "hasGeospatialIssue", "value": "false" }
+      ]
+    },
+    highlightedFilters: ['taxonKey', 'recordedBy', 'country', 'stateProvince', 'gadmGid', 'datasetKey', 'basisOfRecord', 'typeStatus', 'isInCluster', 'occurrenceIssue'],
+    occurrenceSearchTabs: ['TABLE', 'MAP', 'GALLERY', 'DATASETS']
+  }
 };
